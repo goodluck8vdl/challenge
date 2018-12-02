049a27a3f0ca7363976ac3f9eb37e1ab77ee6f0f
Inna Babiichuk <innababiichuk@Innas-MacBook-Pro.local>
2018-12-02 16:36:09 -0500

    first commit

--- server.js
+++ server.js
@@ -3,21 +3,16 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-// app.use(express.static(__dirname+'/public'));
 app.use('/', express.static(path.resolve('./')))
 
 app.use(bodyParser.json()); 
 
-const lessons = require('./channel.json')
+const lessons = require('./src/assets/data/channel.json')
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,'/src/index.html'))
     console.log('get')
 });
-// app.get('*', function (req, res) {
-//     res.sendFile(__dirname + '/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-// });
-
 
 
 app.get('/api/lessons', function (req, res) {
@@ -36,53 +31,6 @@ app.listen(3000, function () {
 });
 module.exports = app;
 
-
-
-
-
-
-
-
-
-// const fs = require('fs');
-// const http = require('http');
-// const router = express.Router();
-// const title = require('./channel.json')
-
-// const app = express();
-
-// const posts = require();
-
-// app.listen(4200, (req, res) => {
-//   console.log('Yeeeeay its running')
-// })
-
-// router.get('./', (req, res, next)=>{
-//  const data = title
-//  console.log(data)
-// })
-
-// app.use(express.static('channel.json'))
-// app.post('./notes', (req, res)=>{
-//   res.send('hello')
-// })
-
-// var fs = require('fs');
-// var title = require('./channel.json')
-
-
-// console.log(title)
-
-// const content = fs.readFileSync('channel.json', 'utf-8');
-// var content = require('channel.json', 'utf-8');
-
-// console.log(content)
-
-// const fs = require('fs');
-
-// let rawdata = fs.readFileSync('channel.json');  
-// let student = JSON.parse(rawdata);  
-// console.log(student); 
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,8 @@ import { LessonsService } from './lessons.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
-
 import { AppComponent } from './app.component';
 import { GroupByPipe } from './app.component';
-
 
 @NgModule({
   declarations: [
